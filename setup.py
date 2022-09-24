e98461d88805732e12e96e980e0c64f1b5f285e0
leandrohorizon <leandro.mcontato@gmail.com>
2022-09-24 18:25:32 -0300

    Corrigindo setup e init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='jargo',
     packages=find_packages(include=[
-      'robot'
+      'jargo'
     ]),
     install_requires=[],
     version='0.1.0',
