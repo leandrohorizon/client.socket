98350e743e72393cb9c84dd16a9634a80641333c
leandrohorizon <leandro.mcontato@gmail.com>
2022-09-25 19:52:25 -0300

    Refletindo mudanças na API

--- build/lib/jargo/robot.py
+++ build/lib/jargo/robot.py
@@ -16,27 +16,38 @@ class Robot():
     )
 
     response_json = json.loads(request.content)
-    interaction = response_json['interaction']
 
-    if(interaction['id'] is None):
-      print('Sem resposta')
+    if request.status_code == 404:
+      self.response = response_json['message']
       return
 
-    reaction = interaction['reaction']
-    Robot.previous_reaction_id = reaction['id']
+    if request.status_code != 200:
+      self.response = "I'm without access to my memory, try again later, sorry"
+      return
 
-    for(procedure) in reaction['procedures']:
-      procedure = procedure['procedure']
+    interaction = response_json['interaction']
+
+    reaction = interaction['reaction']
+
+    Robot.previous_reaction_id = reaction['id']
+    self.response = reaction['text']
+    self.procedures = reaction['procedures']
+
+    return reaction['text']
+
+  def get_response(self):
+    return self.response
+
+  def execute_procedures(self):
+    for(procedure) in self.procedures:
       command = procedure['command']
 
       if not command in Robot.procedures:
-        return
+        return f'not_implemented: {command}'
 
+    for(procedure) in self.procedures:
       arguments = procedure['arguments']
-
       Robot.procedures[command](arguments)
-
-    return reaction['text']
 
   def exec(self, name):
     def decorator(function):
